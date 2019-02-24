60f2ce7d94034a8dc4f7d1adc74ed226d4ee0328
Adel Almaleh <adel.almaleh@resolutions.com.my>
2019-02-24 20:35:15 +0100

    Finalising from the project.

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-
 const data = [{
     realName: 'Brianna Forbes',
     playerName: 'Dreamlurk The Unstoppable',
@@ -34,7 +33,6 @@ const data = [{
 function loadCards() {
     let s = '';
     for (i in data) {
-        // s +='<div>';
         s += '<div class="col-xs-12 col-md-4 col-md-3 clickableCard" id="clickableCard"  data-realName="'+ data[i].realName +'" data-playerName="'+data[i].playerName+'" data-asset="'+data[i].asset+'">';
         s+= '<div class="image-flip">';
         s+= '<div class="mainflip">';
@@ -59,6 +57,7 @@ $(document).on('click', '.clickableCard', function(){
     /*
         Add active class when selected. and defauly color is black
     */
+
     $('.clickableCard').each(function(){
         $(this).attr('style', 'color:black');
         $(this).removeClass('active');
@@ -67,8 +66,9 @@ $(document).on('click', '.clickableCard', function(){
     /*
         Change the card color when selected.
     */
+
     $(this).addClass('active');
-    $(this).attr('style', 'color: #FF7600;');
+    $(this).attr('style', 'color: #FF7600; font-weight: bold');
 
     /*
         Details desplay
@@ -87,25 +87,10 @@ $(document).on('click', '.clickableCard', function(){
 })
 
 /*
-    Submit function. Not im Ajax.
-*/
-$(document).on('click', '#submit', function(){ 
-    $('.clickableCard').each(function(){
-        if ( $(this).hasClass("active") ) {
-            const realName = $(this).data('realname')
-            const playerName = $(this).data('playername')
-            const asset = $(this).data('asset')
-            alert('submited succsessfuly with Real Name: '+realName+', Player Name: '+playerName+', Asset: '+asset)
-        }
-    })
-});
-
-/*
-    Sory names by ascending
+    Sort according to Real name ascending.
 */
 
 $(document).on('click', '#sort_asc', function(){ 
-    const realName = $(this).data('realname')
     var cards = $('div#cards');
     var cont = cards.children('div');
     cont.detach().sort(function(a, b) {
@@ -117,18 +102,56 @@ $(document).on('click', '#sort_asc', function(){
 });
 
 /*
-    Sory names by descending
+    Sort according to Real name descending
 */
 
 $(document).on('click', '#sort_desc', function(){ 
-    const realName = $(this).data('realname')
-    var cards = $('div#cards');
+    var cards = $('div#cards'); 
     var cont = cards.children('div');
     cont.detach().sort(function(b, a) {
-        var astts = $(b).data('realname');
-        var bstts = $(a).data('realname')
+        var astts = $(b).data('realName');
+        var bstts = $(a).data('realName')
         return (astts > bstts) ? (astts > bstts) ? 1 : 0 : -1;
     });
     cards.append(cont);
 });
 
+/*
+    Submit function with Ajax.
+
+    Currently the Ajax does not work but this is the way of doing it.
+*/
+
+$(document).on('click', '#submit', function(){ 
+    $('.clickableCard').each(function(){
+        if ( $(this).hasClass("active") ) {
+            const realName = $(this).data('realname')
+            const playerName = $(this).data('playername')
+            const asset = $(this).data('asset')
+            $.post( "ajax/test", {'realName': realName, 'playerName': playerName, 'asset': asset}, function( err, data ) {
+                alert('submited succsessfuly with Real Name: '+realName+', Player Name: '+ playerName+', Asset: '+asset)
+                if(err){
+                    alert('An error happened. Please try again later.')
+                }
+                alert('Submited successfully')
+            });
+        }
+    })
+});
+
+/*
+    this is another way for the Submit function but without using Ajax.
+*/
+
+/*
+$(document).on('click', '#submit', function(){ 
+    $('.clickableCard').each(function(){
+        if ( $(this).hasClass("active") ) {
+            const realName = $(this).data('realname')
+            const playerName = $(this).data('playername')
+            const asset = $(this).data('asset')
+            alert('submited succsessfuly with Real Name: '+realName+', Player Name: '+playerName+', Asset: '+asset)
+        }
+    })
+});
+*/
